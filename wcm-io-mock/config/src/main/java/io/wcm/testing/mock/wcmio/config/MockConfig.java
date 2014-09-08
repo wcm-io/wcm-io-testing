fc6ac69e9a2431b9a17e3121dc2eb46dd3d3c51e
sseifert <sseifert@pro-vision.de>
2014-09-08 23:09:10 +0200

    add abstract spi implementations for convenience

--- wcm-io-mock/config/src/main/java/io/wcm/testing/mock/wcmio/config/MockConfig.java
+++ wcm-io-mock/config/src/main/java/io/wcm/testing/mock/wcmio/config/MockConfig.java
@@ -19,6 +19,10 @@
  */
 package io.wcm.testing.mock.wcmio.config;
 
+import io.wcm.config.api.Parameter;
+import io.wcm.config.api.management.ParameterPersistence;
+import io.wcm.config.api.management.PersistenceException;
+import io.wcm.config.core.impl.ApplicationImplementationPicker;
 import io.wcm.config.core.impl.ConfigurationAdapterFactory;
 import io.wcm.config.core.management.impl.ApplicationFinderImpl;
 import io.wcm.config.core.management.impl.ConfigurationFinderImpl;
@@ -26,8 +30,18 @@ import io.wcm.config.core.management.impl.ParameterOverrideImpl;
 import io.wcm.config.core.management.impl.ParameterPersistenceImpl;
 import io.wcm.config.core.management.impl.ParameterResolverImpl;
 import io.wcm.config.core.persistence.ToolsConfigPagePersistenceProvider;
+import io.wcm.config.spi.ApplicationProvider;
+import io.wcm.config.spi.ConfigurationFinderStrategy;
+import io.wcm.config.spi.ParameterProvider;
+import io.wcm.config.spi.helpers.AbstractAbsoluteParentConfigurationFinderStrategy;
+import io.wcm.config.spi.helpers.AbstractParameterProvider;
+import io.wcm.config.spi.helpers.AbstractPathApplicationProvider;
 import io.wcm.testing.mock.aem.junit.AemContext;
 
+import java.util.Map;
+import java.util.Set;
+
+import com.day.jcr.vault.util.Text;
 import com.google.common.collect.ImmutableMap;
 
 /**
@@ -61,6 +75,77 @@ public final class MockConfig {
     // adapter factory
     context.registerInjectActivateService(new ConfigurationAdapterFactory());
 
+    // models implementation picker
+    context.registerInjectActivateService(new ApplicationImplementationPicker());
+
+  }
+
+  /**
+   * {@link ConfigurationFinderStrategy} that supports one or multiple fixed levels in content hierarchy where
+   * configurations are supported.
+   * @param applicationId Application ID
+   * @param levels List of absolute levels where configuration is supported.
+   *          Levels are used in the same way as {@link Text#getAbsoluteParent(String, int)}.
+   *          Example:<br>
+   *          <code>Text.getAbsoluteParent("/foo/bar/test", 1) == "/foo/bar"</code>
+   * @return Configuration finder strategy
+   */
+  public static ConfigurationFinderStrategy configurationFinderStrategyAbsoluteParent(
+      final String applicationId, final int... levels) {
+    return new AbstractAbsoluteParentConfigurationFinderStrategy(applicationId, levels) {
+      // nothing to override
+    };
+  }
+
+  /**
+   * {@link ParameterProvider} providing list of parameters from given parameter set.
+   * @param parameters Parameter set
+   * @return Parameter provider
+   */
+  public static ParameterProvider parameterProvider(final Set<Parameter<?>> parameters) {
+    return new AbstractParameterProvider(parameters) {
+      // nothing to override
+    };
+  }
+
+  /**
+   * {@link ParameterProvider} providing list of parameters from given from reading all public static fields from a
+   * given class definition.
+   * @param type Class definition
+   * @return Parameter provider
+   */
+  public static ParameterProvider parameterProvider(final Class<?> type) {
+    return new AbstractParameterProvider(type) {
+      // nothing to override
+    };
+  }
+
+  /**
+   * {@link ApplicationProvider} that supports detecting an application based on one or multiple fixed paths subtrees.
+   * @param applicationId Application id
+   * @param paths List of paths/subtrees this application belongs to
+   * @return Application provider
+   */
+  public static ApplicationProvider applicationProvider(final String applicationId, final String... paths) {
+    return new AbstractPathApplicationProvider(applicationId, applicationId, paths) {
+      // nothing to override
+    };
+  }
+
+  /**
+   * Writes configuration parameters using the primary configured persistence provider.
+   * @param context AEM context
+   * @param configurationId Configuration id
+   * @param values Configuration values
+   */
+  public static void writeConfiguration(AemContext context, String configurationId, Map<String, Object> values) {
+    try {
+      ParameterPersistence persistence = context.slingScriptHelper().getService(ParameterPersistence.class);
+      persistence.storeParameterValues(context.resourceResolver(), configurationId, values);
+    }
+    catch (PersistenceException ex) {
+      throw new RuntimeException("Storing parameter values for " + configurationId + " failed.", ex);
+    }
   }
 
 }
