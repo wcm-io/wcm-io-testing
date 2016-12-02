0d167e9dc8306e807b49f514a7e7f70a27fa4407
sseifert <sseifert@pro-vision.de>
2016-12-02 23:36:48 +0100

    caconfig mock

---
+++ wcm-io-mock/caconfig/src/main/java/io/wcm/testing/mock/wcmio/config/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2016 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/**
+ * Helps setting up mock environment for wcm.io Configuration.
+ */
+@org.osgi.annotation.versioning.Version("1.0.0")
+package io.wcm.testing.mock.wcmio.config;
