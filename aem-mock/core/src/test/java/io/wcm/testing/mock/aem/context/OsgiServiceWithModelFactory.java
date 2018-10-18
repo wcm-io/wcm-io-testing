0d9e9cb6c1b191e5d625f884ff3335c0a040f291
sseifert <sseifert@pro-vision.de>
2018-10-18 19:02:08 +0200

    WTEST-35 reproduce problem from ticket

---
+++ aem-mock/core/src/test/java/io/wcm/testing/mock/aem/context/OsgiServiceWithModelFactory.java
@@ -0,0 +1,39 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2018 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.testing.mock.aem.context;
+
+import org.apache.sling.models.factory.ModelFactory;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * Sample service referencing ModelFactory.
+ */
+@Component(service = OsgiServiceWithModelFactory.class)
+public class OsgiServiceWithModelFactory {
+
+  @Reference
+  private ModelFactory modelFactory;
+
+  public ModelFactory getModelFactory() {
+    return this.modelFactory;
+  }
+
+}
