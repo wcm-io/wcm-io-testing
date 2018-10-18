54124df882a8aa6e4432782930cb5a553ed6f981
sseifert <sseifert@pro-vision.de>
2018-10-18 10:44:56 +0200

    add test cases to validate WTES-35

---
+++ aem-mock/core/src/test/java/io/wcm/testing/mock/aem/modelsautoreg/ModelWithModelFactory.java
@@ -0,0 +1,40 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2018 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.testing.mock.aem.modelsautoreg;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.factory.ModelFactory;
+
+/**
+ * Test sling model with reference to ModelFactory service.
+ */
+@Model(adaptables = SlingHttpServletRequest.class)
+public class ModelWithModelFactory {
+
+  @OSGiService
+  private ModelFactory modelFactory;
+
+  public ModelFactory getModelFactory() {
+    return this.modelFactory;
+  }
+
+}
