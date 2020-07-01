82b99ed6d1e579be4e46f8dc42f289d311ef03ab
sseifert <sseifert@pro-vision.de>
2020-07-01 22:09:09 +0200

    reformat code

--- aem-mock/core/src/main/java/io/wcm/testing/mock/aem/MockLanguageManager.java
+++ aem-mock/core/src/main/java/io/wcm/testing/mock/aem/MockLanguageManager.java
@@ -31,238 +31,234 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
-import com.day.cq.commons.jcr.JcrConstants;
-import com.day.cq.wcm.api.PageManager;
-import com.day.text.Text;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-
-import com.day.cq.commons.Language;
-import com.day.cq.commons.LanguageUtil;
-import com.day.cq.wcm.api.LanguageManager;
-import com.day.cq.wcm.api.Page;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.osgi.annotation.versioning.ProviderType;
 import org.osgi.service.component.annotations.Component;
 
+import com.day.cq.commons.Language;
+import com.day.cq.commons.LanguageUtil;
+import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
+import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.wcm.api.LanguageManager;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import com.day.text.Text;
+
 @Component(service = LanguageManager.class)
 @ProviderType
 public class MockLanguageManager implements LanguageManager {
 
-    @Override
-    public String getIsoCountry(final Locale locale) {
-        throw new UnsupportedOperationException();
+  @Override
+  public String getIsoCountry(final Locale locale) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  @Deprecated
+  public Map<Locale, Info> getAdjacentInfo(final ResourceResolver resourceResolver, final String path) {
+    return Optional.ofNullable(getAdjacentLanguageInfo(resourceResolver, path))
+        .map(Map::entrySet)
+        .map(Collection::stream)
+        .map(entries -> entries.collect(toLinkedMap(i -> i.getKey().getLocale(), Map.Entry::getValue)))
+        .orElse(null);
+  }
+
+  @Override
+  public Map<Language, Info> getAdjacentLanguageInfo(final ResourceResolver resourceResolver, final String path) {
+    return Optional.ofNullable(LanguageUtil.getLanguageRoot(path))
+        .map(root -> path.substring(root.length()))
+        .map(relPath -> relPath.startsWith("/") ? relPath.substring(1) : relPath)
+        .map(relPath -> this.getLanguageRootStream(resourceResolver, path)
+            .map(info -> info.getChild(relPath, resourceResolver))
+            .collect(toLinkedMap(InfoImpl::getLanguage, i -> (Info)i)))
+        .orElse(null);
+  }
+
+  @Override
+  public Locale getLanguage(final Resource resource) {
+    return this.getLanguage(resource, true);
+  }
+
+  @Override
+  public Language getCqLanguage(final Resource resource) {
+    return this.getCqLanguage(resource, true);
+  }
+
+  @Override
+  public Locale getLanguage(final Resource resource, final boolean respectContent) {
+    return Optional.ofNullable(getCqLanguage(resource, respectContent))
+        .map(Language::getLocale)
+        .orElse(null);
+  }
+
+  @Override
+  public Language getCqLanguage(final Resource resource, final boolean respectContent) {
+    Optional<Page> page = Optional.ofNullable(resource.getResourceResolver().adaptTo(PageManager.class))
+        .map(pm -> pm.getContainingPage(resource));
+
+    if (respectContent) {
+      return page
+          .map(Page::getContentResource)
+          .map(HierarchyNodeInheritanceValueMap::new)
+          .map(vm -> vm.getInherited(JcrConstants.JCR_LANGUAGE, String.class))
+          .map(LanguageUtil::getLanguage)
+          .orElseGet(() -> this.getCqLanguage(resource, false));
+    }
+
+    return page
+        .map(Page::getPath)
+        .map(LanguageUtil::getLanguageRoot)
+        .map(Text::getName)
+        .map(Language::new)
+        .orElse(null);
+  }
+
+  @Override
+  public Page getLanguageRoot(final Resource resource) {
+    return Optional.ofNullable(LanguageUtil.getLanguageRoot(resource.getPath()))
+        .map(resource.getResourceResolver()::getResource)
+        .map(res -> res.adaptTo(Page.class))
+        .orElse(null);
+  }
+
+  @Override
+  public Collection<Locale> getLanguages(final ResourceResolver resourceResolver, final String path) {
+    return this.getCqLanguages(resourceResolver, path).stream()
+        .map(Language::getLocale)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public Collection<Language> getCqLanguages(final ResourceResolver resourceResolver, final String path) {
+    return this.getLanguageRootStream(resourceResolver, path)
+        .map(InfoImpl::getLanguage)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public Collection<Page> getLanguageRoots(final ResourceResolver resourceResolver, final String path) {
+    return this.getLanguageRootStream(resourceResolver, path)
+        .map(InfoImpl::getResource)
+        .filter(Objects::nonNull)
+        .map(res -> res.adaptTo(Page.class))
+        .filter(Objects::nonNull)
+        .collect(Collectors.toList());
+  }
+
+  private Stream<InfoImpl> getLanguageRootStream(final ResourceResolver resourceResolver, final String path) {
+    return Optional.ofNullable(LanguageUtil.getLanguageRoot(path))
+        .map(resourceResolver::getResource)
+        .map(Resource::getParent)
+        .map(Resource::listChildren)
+        .map(childIterator -> StreamSupport.stream(((Iterable<Resource>)() -> childIterator).spliterator(), false))
+        .orElseGet(Stream::empty)
+        .filter(res -> Objects.nonNull(LanguageUtil.getLanguage(res.getName())))
+        .map(res -> new InfoImpl(res.getPath(), res, LanguageUtil.getLanguage(res.getName())));
+  }
+
+  @Override
+  public Tree compareLanguageTrees(final ResourceResolver resourceResolver, final String path) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Collector for collecting a stream to a linked hash map.
+   * @param keyMapper A mapping function to produce keys.
+   * @param valueMapper A mapping function to produce values.
+   * @param <T> The type of input elements to the reduction operation.
+   * @param <K> The output type of the key mapping function.
+   * @param <U> The output type of the value mapping function.
+   * @return A Collector which collects elements into a LinkedHashMap whose keys are the result of applying a key
+   *         mapping function to the input elements, and whose values are the result of applying a value mapping
+   *         function to
+   *         all input elements equal to the key and combining them using the merge function
+   */
+  private static <T, K, U> Collector<T, ?, Map<K, U>> toLinkedMap(
+      final Function<? super T, ? extends K> keyMapper, final Function<? super T, ? extends U> valueMapper) {
+    return Collectors.toMap(
+        keyMapper,
+        valueMapper,
+        (u, v) -> {
+          throw new IllegalStateException(String.format("Duplicate key %s", u));
+        },
+        LinkedHashMap::new);
+  }
+
+  private static final class InfoImpl implements LanguageManager.Info {
+
+    private final String path;
+    private final Resource resource;
+    private final Language language;
+
+    public InfoImpl(@NotNull final String path, @Nullable final Resource resource, @NotNull final Language language) {
+      this.path = path;
+      this.resource = resource;
+      this.language = language;
     }
 
     @Override
-    @Deprecated
-    public Map<Locale, Info> getAdjacentInfo(final ResourceResolver resourceResolver, final String path) {
-        return Optional.ofNullable(getAdjacentLanguageInfo(resourceResolver, path))
-                .map(Map::entrySet)
-                .map(Collection::stream)
-                .map(entries -> entries.collect(toLinkedMap(i -> i.getKey().getLocale(), Map.Entry::getValue)))
-                .orElse(null);
+    public String getPath() {
+      return this.path;
     }
 
     @Override
-    public Map<Language, Info> getAdjacentLanguageInfo(final ResourceResolver resourceResolver, final String path) {
-        return Optional.ofNullable(LanguageUtil.getLanguageRoot(path))
-                .map(root -> path.substring(root.length()))
-                .map(relPath -> relPath.startsWith("/") ? relPath.substring(1) : relPath)
-                .map(relPath ->
-                        this.getLanguageRootStream(resourceResolver, path)
-                                .map(info -> info.getChild(relPath, resourceResolver))
-                                .collect(toLinkedMap(InfoImpl::getLanguage, i -> (Info) i)))
-                .orElse(null);
+    public boolean exists() {
+      return this.resource != null;
     }
 
     @Override
-    public Locale getLanguage(final Resource resource) {
-        return this.getLanguage(resource, true);
+    public boolean hasContent() {
+      return Optional.ofNullable(this.resource)
+          .map(res -> resource.getChild(JcrConstants.JCR_CONTENT))
+          .isPresent();
     }
 
     @Override
-    public Language getCqLanguage(final Resource resource) {
-        return this.getCqLanguage(resource, true);
-    }
-
-    @Override
-    public Locale getLanguage(final Resource resource, final boolean respectContent) {
-        return Optional.ofNullable(getCqLanguage(resource, respectContent))
-                .map(Language::getLocale)
-                .orElse(null);
-    }
-
-    @Override
-    public Language getCqLanguage(final Resource resource, final boolean respectContent) {
-        Optional<Page> page = Optional.ofNullable(resource.getResourceResolver().adaptTo(PageManager.class))
-                .map(pm -> pm.getContainingPage(resource));
-
-        if (respectContent) {
-            return page
-                    .map(Page::getContentResource)
-                    .map(HierarchyNodeInheritanceValueMap::new)
-                    .map(vm -> vm.getInherited(JcrConstants.JCR_LANGUAGE, String.class))
-                    .map(LanguageUtil::getLanguage)
-                    .orElseGet(() -> this.getCqLanguage(resource, false));
-        }
-
-        return page
-                .map(Page::getPath)
-                .map(LanguageUtil::getLanguageRoot)
-                .map(Text::getName)
-                .map(Language::new)
-                .orElse(null);
-    }
-
-    @Override
-    public Page getLanguageRoot(final Resource resource) {
-        return Optional.ofNullable(LanguageUtil.getLanguageRoot(resource.getPath()))
-                .map(resource.getResourceResolver()::getResource)
-                .map(res -> res.adaptTo(Page.class))
-                .orElse(null);
-    }
-
-    @Override
-    public Collection<Locale> getLanguages(final ResourceResolver resourceResolver, final String path) {
-        return this.getCqLanguages(resourceResolver, path).stream()
-                .map(Language::getLocale)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public Collection<Language> getCqLanguages(final ResourceResolver resourceResolver, final String path) {
-        return this.getLanguageRootStream(resourceResolver, path)
-                .map(InfoImpl::getLanguage)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public Collection<Page> getLanguageRoots(final ResourceResolver resourceResolver, final String path) {
-        return this.getLanguageRootStream(resourceResolver, path)
-                .map(InfoImpl::getResource)
-                .filter(Objects::nonNull)
-                .map(res -> res.adaptTo(Page.class))
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-    }
-
-    private Stream<InfoImpl> getLanguageRootStream(final ResourceResolver resourceResolver, final String path) {
-        return Optional.ofNullable(LanguageUtil.getLanguageRoot(path))
-                .map(resourceResolver::getResource)
-                .map(Resource::getParent)
-                .map(Resource::listChildren)
-                .map(childIterator -> StreamSupport.stream(((Iterable<Resource>) () -> childIterator).spliterator(), false))
-                .orElseGet(Stream::empty)
-                .filter(res -> Objects.nonNull(LanguageUtil.getLanguage(res.getName())))
-                .map(res -> new InfoImpl(res.getPath(), res, LanguageUtil.getLanguage(res.getName())));
-    }
-
-    @Override
-    public Tree compareLanguageTrees(final ResourceResolver resourceResolver, final String path) {
-        throw new UnsupportedOperationException();
+    public long getLastModified() {
+      return Optional.ofNullable(this.resource)
+          .map(res -> resource.getChild(JcrConstants.JCR_CONTENT))
+          .map(Resource::getValueMap)
+          .map(vm -> vm.get(JcrConstants.JCR_LASTMODIFIED, Long.class))
+          .orElse(0L);
     }
 
     /**
-     * Collector for collecting a stream to a linked hash map.
-     *
-     * @param keyMapper   A mapping function to produce keys.
-     * @param valueMapper A mapping function to produce values.
-     * @param <T>         The type of input elements to the reduction operation.
-     * @param <K>         The output type of the key mapping function.
-     * @param <U>         The output type of the value mapping function.
-     * @return A Collector which collects elements into a LinkedHashMap whose keys are the result of applying a key
-     * mapping function to the input elements, and whose values are the result of applying a value mapping function to
-     * all input elements equal to the key and combining them using the merge function
+     * The resource located at {@link #getPath()}, if it exists.
+     * @return The resource.
      */
-    private static <T, K, U> Collector<T, ?, Map<K, U>> toLinkedMap(
-            final Function<? super T, ? extends K> keyMapper, final Function<? super T, ? extends U> valueMapper) {
-        return Collectors.toMap(
-                keyMapper,
-                valueMapper,
-                (u, v) -> {
-                    throw new IllegalStateException(String.format("Duplicate key %s", u));
-                },
-                LinkedHashMap::new
-        );
+    @Nullable
+    private Resource getResource() {
+      return this.resource;
     }
 
-    private static final class InfoImpl implements LanguageManager.Info {
-        private final String path;
-        private final Resource resource;
-        private final Language language;
-
-        public InfoImpl(@NotNull final String path, @Nullable final Resource resource, @NotNull final Language language) {
-            this.path = path;
-            this.resource = resource;
-            this.language = language;
-        }
-
-        @Override
-        public String getPath() {
-            return this.path;
-        }
-
-        @Override
-        public boolean exists() {
-            return this.resource != null;
-        }
-
-        @Override
-        public boolean hasContent() {
-            return Optional.ofNullable(this.resource)
-                    .map(res -> resource.getChild(JcrConstants.JCR_CONTENT))
-                    .isPresent();
-        }
-
-        @Override
-        public long getLastModified() {
-            return Optional.ofNullable(this.resource)
-                    .map(res -> resource.getChild(JcrConstants.JCR_CONTENT))
-                    .map(Resource::getValueMap)
-                    .map(vm -> vm.get(JcrConstants.JCR_LASTMODIFIED, Long.class))
-                    .orElse(0L);
-        }
-
-        /**
-         * The resource located at {@link #getPath()}, if it exists.
-         *
-         * @return The resource.
-         */
-        @Nullable
-        private Resource getResource() {
-            return this.resource;
-        }
-
-        /**
-         * Get the language.
-         *
-         * @return The language.
-         */
-        @NotNull
-        private Language getLanguage() {
-            return this.language;
-        }
-
-        /**
-         * Gets the InfoImpl for a child resource under the current InfoImpl's path.
-         * <p>
-         * This constructs a new InfoImpl using the path getPath() + / + relPath.
-         *
-         * @param relPath          Path relative to the current path.
-         * @param resourceResolver A resource resolver.
-         * @return A new InfoImpl for the resource specified at relPath.
-         */
-        private InfoImpl getChild(@NotNull final String relPath, @NotNull final ResourceResolver resourceResolver) {
-            if (relPath.isEmpty()) {
-                return this;
-            }
-            String path = String.join("/", this.path, relPath);
-            Resource child = resourceResolver.getResource(path);
-            return new InfoImpl(path, child, this.getLanguage());
-        }
+    /**
+     * Get the language.
+     * @return The language.
+     */
+    @NotNull
+    private Language getLanguage() {
+      return this.language;
     }
+
+    /**
+     * Gets the InfoImpl for a child resource under the current InfoImpl's path.
+     * <p>
+     * This constructs a new InfoImpl using the path getPath() + / + relPath.
+     * @param relPath Path relative to the current path.
+     * @param resourceResolver A resource resolver.
+     * @return A new InfoImpl for the resource specified at relPath.
+     */
+    private InfoImpl getChild(@NotNull final String relPath, @NotNull final ResourceResolver resourceResolver) {
+      if (relPath.isEmpty()) {
+        return this;
+      }
+      String path = String.join("/", this.path, relPath);
+      Resource child = resourceResolver.getResource(path);
+      return new InfoImpl(path, child, this.getLanguage());
+    }
+  }
 }
 
